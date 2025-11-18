dc321aee60cc7145352ca589fa1e155c9257f3a5
Fatihture <fatihtureee@gmail.com>
2025-11-18 18:31:10 +0300

    app.css dosyası componentlere bölündü

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
@@ -5,45 +6,15 @@ import Order from './pages/Order.jsx';
 import Success from './pages/Success.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
-import './App.css';
+import './styles/Home.css';
+import './styles/order.css';
+import './styles/success.css';
+
+// import './App.css';
 
 function App() {
   const [orderData, setOrderData] = useState(null);
   const [apiResponse, setApiResponse] = useState(null);
-
-  // IT2: Ana sayfa için dinamik kategori ve ürün verileri
-  const categories = [
-    { id: 1, label: 'Ramen', icon: '/images/iteration-2-images/icons/1.svg' },
-    { id: 2, label: 'Pizza', icon: '/images/iteration-2-images/icons/2.svg' },
-    { id: 3, label: 'Burger', icon: '/images/iteration-2-images/icons/3.svg' },
-    { id: 4, label: 'Kızartmalar', icon: '/images/iteration-2-images/icons/4.svg' },
-    { id: 5, label: 'Fast Food', icon: '/images/iteration-2-images/icons/5.svg' },
-    { id: 6, label: 'Soft Drink', icon: '/images/iteration-2-images/icons/6.svg' },
-  ];
-
-  const popularFoods = [
-    {
-      id: 1,
-      title: 'Terminal Pizza',
-      price: '60₺',
-      image: '/images/iteration-2-images/pictures/food-1.png',
-      category: 'Pizza',
-    },
-    {
-      id: 2,
-      title: 'Position Absolute Acı Pizza',
-      price: '85.50₺',
-      image: '/images/iteration-2-images/pictures/food-2.png',
-      category: 'Pizza',
-    },
-    {
-      id: 3,
-      title: 'useEffect Tavuklu Burger',
-      price: '70₺',
-      image: '/images/iteration-2-images/pictures/food-3.png',
-      category: 'Burger',
-    },
-  ];
 
   return (
     <div className="app">
@@ -51,7 +22,7 @@ function App() {
       <main className="main">
         <Switch>
           <Route exact path="/">
-            <Home categories={categories} popularFoods={popularFoods} />
+            <Home />
           </Route>
 
           <Route path="/order">
